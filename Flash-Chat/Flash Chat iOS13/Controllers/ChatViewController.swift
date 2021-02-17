3e12c74fb9b8fd1b2427f25347e963800a8575a7
Cao Thành Nhân <55010539+ctnlegend@users.noreply.github.com>
2021-02-18 02:06:19 +0700

    Fix bug, hoàn thiện UI,...

--- Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -39,6 +39,8 @@ class ChatViewController: UIViewController{
                                     self.messages.append(newMessage)
                                     DispatchQueue.main.async {
                                         self.tableView.reloadData()
+                                        let indexPath=IndexPath(row: self.messages.count - 1, section: 0)
+                                        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                         }
                     }
                 }
@@ -58,6 +60,9 @@ class ChatViewController: UIViewController{
                 }
                 else{
                     print("Firestore OK!")
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text=""
+                    }
                 }
             }
         }
@@ -80,8 +85,26 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label.text=messages[indexPath.row].body
+        cell.label.text = message.body
+        
+        //currentUser messages
+        if message.sender == Auth.auth().currentUser?.email{
+            cell.rightImageView.isHidden=false
+            cell.leftImageView.isHidden=true
+            cell.messageBubble.backgroundColor=UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor=UIColor(named: K.BrandColors.purple)
+        }
+        //anotherUser messages
+        else{
+            cell.rightImageView.isHidden=true
+            cell.leftImageView.isHidden=false
+            cell.messageBubble.backgroundColor=UIColor(named: K.BrandColors.purple)
+            cell.label.textColor=UIColor(named: K.BrandColors.lightPurple)
+        }
         return cell
     }
     
